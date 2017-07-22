48f4dd5546e2bf95119d473db171a440a14be95e
Vrungel <nikita241296@gmail.com>
2017-07-22 12:51:24 +0300

    init

---
+++ app/src/main/java/com/example/vrungel/smartyads/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vrungel.smartyads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
